5343a334dd49dcc911aa4c653318da3b0d29d1c7
Ian Zhang <ianzhang1986@gmail.com>
2022-06-04 18:03:29 -0700

    Update delay2.v

--- delay2.v
+++ delay2.v
@@ -1,64 +1,51 @@
-// Derived from Intel example
-`ifndef UNSIG_ALTMULT_ACCUM_V_
-`define UNSIG_ALTMULT_ACCUM_V_
+`ifndef DELAY_2_V_
+`define DELAY_2_V_
 
 `timescale 1ns / 1ps
 `default_nettype none
 
-module unsig_altmult_accum # (
-  parameter W = 32
+module delay2 #(
+  parameter WIDTH = 32,
+  parameter DEPTH = 3
 ) (
-  input wire [    W - 1 : 0] dataa,
-  input wire [    W - 1 : 0] datab,
-  input wire                 clk,
-  input wire                 aclr,      // async clear
-  input wire                 clken,     // clk enable
-  input wire                 sload,     // sync load
-  output reg [2 * W - 1 : 0] adder_out
+  input  wire                 clk,
+  input  wire                 reset,
+  input  wire [WIDTH - 1 : 0] data_in,
+  output wire [WIDTH - 1 : 0] data_out
 );
 
-  // Declare registers and wires
-  reg  [2 * W - 1:0] dataa_reg;
-  reg  [2 * W - 1:0] datab_reg;
-  reg                sload_reg;
-  reg  [2 * W - 1:0] old_result;
-  wire [2 * W - 1:0] multa;
+  wire [WIDTH - 1 : 0] connect_wire [DEPTH : 0];
 
-  // Store the results of the operations on the current data
-  //assign multa = dataa_reg * datab_reg;
-  assign multa = dataa_reg;
+  assign data_out        = connect_wire[DEPTH];
+  assign connect_wire[0] = data_in;
 
-  // Store the value of the accumulation (or clear it)
-  always @ (adder_out, sload_reg)
-  begin
-    if (sload_reg)
-      old_result <= 0;
-    else
-      old_result <= adder_out;
+  genvar i;
+  generate
+    for (i = 1; i <= DEPTH; i = i + 1) begin
+      dff #(.WIDTH(WIDTH)) DFF(
+        .clk(clk),
+        .rst(reset),
+        .inp(connect_wire[i-1]),
+        .outp(connect_wire[i]));
     end
+  endgenerate
+endmodule
 
-    // Clear or update data, as appropriate
-    always @ (posedge clk or posedge aclr)
-    begin
-      if (aclr)
-      begin
-        dataa_reg <= 0;
-        datab_reg <= 0;
-        sload_reg <= 0;
-        adder_out <= 0;
-      end
-      else if (clken)
-      begin
-        dataa_reg <= dataa;
-        datab_reg <= datab;
-        sload_reg <= sload;
-        adder_out <= old_result + multa;
-      end
-      else begin
-        dataa_reg <= 32'bz;
-        datab_reg <= 32'bz;
-      end
-    end
+// D flip-flop with synchronous reset
+module dff#(
+    parameter WIDTH = 1
+  ) (
+    input wire clk,
+    input wire rst,
+
+    input wire [WIDTH-1:0] inp,
+    output reg [WIDTH-1:0] outp
+  );
+
+  always @(posedge clk) begin
+    outp <= rst ? 0 : inp;
+  end
+
 endmodule
 
 `default_nettype wire
