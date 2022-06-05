16b361bc965978a7b9ea6fd0cbe8a5a8b2e77010
Ian Zhang <ianzhang1986@gmail.com>
2022-06-05 10:27:24 -0700

    Update mat_mul_tb.v

--- mat_mul_tb.v
+++ mat_mul_tb.v
@@ -9,15 +9,15 @@ module verification();
   localparam FORCED_DELAY = 5;
   localparam clock_period = 10;
   
-//   bit clk, reset, en;
+  //bit clk, reset, en;
   reg clk, reset, en;
   
-  localparam W = 32;
+  localparam W = 16;
   localparam N = 3;
   localparam BIT_MAT_DIM = W * N * N;
   reg  [    BIT_MAT_DIM - 1 : 0] A_mat;
   reg  [    BIT_MAT_DIM - 1 : 0] B_mat;
-  wire [2 * BIT_MAT_DIM - 1 : 0] C_mat;
+  wire [    BIT_MAT_DIM - 1 : 0] C_mat;
   
   integer iter_cnt;
   
@@ -30,26 +30,52 @@ module verification();
     $dumpfile("dump.vcd");
     $dumpvars(1);
 
-    A_mat = {
+/*    A_mat = {
         32'hffffffff, 32'hffffffff, 32'hffffffff,
         32'hffffffff, 32'hffffffff, 32'hffffffff,
         32'hffffffff, 32'hffffffff, 32'hffffffff
+    };
+*/
+    A_mat = {
+        16'h0f0f, 16'h0f0f, 16'h0f0f,
+        16'h0f0f, 16'h0f0f, 16'h0f0f,
+        16'h0f0f, 16'h0f0f, 16'h0f0f
+    };
+
+/*    B_mat = {
+        32'hffffffff, 32'hffffffff, 32'hffffffff,
+        32'hffffffff, 32'hffffffff, 32'hffffffff,
+        32'hffffffff, 32'hffffffff, 32'hffffffff
+    };
+*/
+
+    B_mat = {
+        16'h0f0f, 16'h0f0f, 16'h0f0f,
+        16'h0f0f, 16'h0f0f, 16'h0f0f,
+        16'h0f0f, 16'h0f0f, 16'h0f0f
+    };
+
+    iter_cnt = 0;    
+    #(clock_period);
+
+    // clear after one cycle
+    A_mat = {
+      16'h0000, 16'h0000, 16'h0000,
+      16'h0000, 16'h0000, 16'h0000,
+      16'h0000, 16'h0000, 16'h0000
     };
 
     B_mat = {
-        32'hffffffff, 32'hffffffff, 32'hffffffff,
-        32'hffffffff, 32'hffffffff, 32'hffffffff,
-        32'hffffffff, 32'hffffffff, 32'hffffffff
+      16'h0000, 16'h0000, 16'h0000,
+      16'h0000, 16'h0000, 16'h0000,
+      16'h0000, 16'h0000, 16'h0000
     };
-    #(clock_period);
-    iter_cnt = 0;    
-    clk      = 1'b1;
+    
+    clk      = 1'b0;
     reset    = 1'b1;
     #(10 * clock_period);
     #(clock_period) reset = 0; en = 0;
     #(clock_period) reset = 0; en = 1;
-    
-    //C_mat = 32'b0;
     
 //     #(FORCED_DELAY * CLK_INT_2) assert_1: assert (C_mat != 32'b0);
   end
@@ -59,11 +85,13 @@ module verification();
   end
   
   always @(iter_cnt) begin
-    if (iter_cnt > 100) begin
+    /*if (iter_cnt > 100) begin
       assert_1 : assert (C_mat != 32'b0);
     end
-    if (iter_cnt == 350) begin
+    */
+    if (iter_cnt >= 350) begin
       $stop;
+      $finish;
     end
   end
   
