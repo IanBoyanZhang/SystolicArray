93a3fe63d04c050efaf44ca54fc43de7084fe93c
Ian Zhang <ianzhang1986@gmail.com>
2022-06-05 10:14:59 -0700

    Update PE2.v

--- PE2.v
+++ PE2.v
@@ -1,47 +1,72 @@
 `ifndef PE_2_V_
 `define PE_2_V_
 
-`include "unsig_altmult_accum.v"
+`include "mac_unit.v"
 `include "delay2.v"
 
 `timescale 1ns / 1ps
 `default_nettype none
 
 module PE #(
-  parameter W = 32
+  //parameter W = 32
+  parameter W = 16
 ) (
   input  wire                 i_clk,
   input  wire                 i_rst,
   input  wire                 i_sync,
   input  wire                 i_en,
+  input  wire                 i_mode,
   input  wire [    W - 1 : 0] i_A,
   input  wire [    W - 1 : 0] i_B,
   output wire [    W - 1 : 0] o_A,
   output wire [    W - 1 : 0] o_B,
-  output wire [2 * W - 1 : 0] o_C
+  //output wire [    W - 1 : 0] o_C
+  output reg  [    W - 1 : 0] o_C
 );
 
-  wire sync_load;
+  //wire mode;
+  //assign mode = 1;
+
+  //wire sync_load;
   assign o_A = i_A;
   assign o_B = i_B;
-  assign sync_load = i_sync | i_rst;
-  
+  //assign sync_load = i_sync | i_rst;
+
   wire [W - 1 : 0] i_A_buffered;
   wire [W - 1 : 0] i_B_buffered;
+
+  reg  [15 : 0] accu;
+  wire [15 : 0] mac_out;
 
   // Buffered in MAC
   delay2 #(.WIDTH(W), .DEPTH(1)) delayA(.clk(i_clk), .reset(i_rst), .data_in(i_A), .data_out(i_A_buffered));
   delay2 #(.WIDTH(W), .DEPTH(1)) delayB(.clk(i_clk), .reset(i_rst), .data_in(i_B), .data_out(i_B_buffered));
-  
-  // Fix
-  unsig_altmult_accum #(.W(W)) u_mac(
-    .dataa(i_A_buffered),
-    .datab(i_B_buffered),
-    .clk(i_clk),
-    .aclr(i_rst),
-    .clken(i_en),
-    .sload(sync_load),
-    .adder_out(o_C)
+
+  always @(i_clk) begin
+    if (i_rst) begin
+      accu <= 1'b0;
+      o_C  <= 0;
+    end
+    else begin
+      accu <= mac_out;
+      o_C  <= mac_out;
+    end
+  end
+
+  always @(i_clk) begin
+    if (i_sync) begin
+      accu <= 1'b0;
+      o_C  <= 0;
+    end
+  end
+
+  // Optional: making it clocked
+  mac_unit u0_mac(
+    .in_a    (i_A_buffered),
+    .in_b    (i_B_buffered),
+    .in_c    (accu),
+    .mode    (i_mode),
+    .mac_out (mac_out)
   );
 
 endmodule
