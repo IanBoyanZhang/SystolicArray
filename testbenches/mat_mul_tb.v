92439d6d4ad2577fca5eafacac373acb111e22c2
Ian Zhang <ianzhang1986@gmail.com>
2022-06-07 13:26:34 -0700

    Update mat_mul_tb.v

--- testbenches/mat_mul_tb.v
+++ testbenches/mat_mul_tb.v
@@ -22,7 +22,6 @@ module verification();
   
   integer iter_cnt;
   
-  wire [W - 1 : 0] o_d_a00;
   wire o_done;
   // clock generation
   always #(clock_period/2) clk <= ~clk;
@@ -84,8 +83,6 @@ module verification();
     .i_A(A_mat),
     .i_B(B_mat),
     .o_C(C_mat),
-    .o_done(o_done),
-    // debug
-    .o_d_a00(o_d_a00)
+    .o_done(o_done)
   );
 endmodule
