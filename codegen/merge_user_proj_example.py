0287016bd40244ff5135fa8d7d03c98231389c69
Ian Zhang <ianzhang1986@gmail.com>
2022-07-31 08:28:41 -0700

    Create merge_user_proj_example.py

---
+++ codegen/merge_user_proj_example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from shutil import copyfile
+import os, errno
+
+copyfile('./user_proj_example.v', './user_proj_example_cp.v')
+
+src_list = [
+        'mat_mul_generated.v',
+        'user_proj_example_cp.v'
+]
+
+# TODO: Passing from cmd arguments which can be used by the makefile
+concat_filepath = 'user_proj_example.v'
+
+with open(concat_filepath, 'w') as outfile:
+    for fname in src_list:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+try:
+    os.remove('./user_proj_example_cp.v')
+except OSError as e:
+    if e.errno != errno.ENOENT:
+        raise
